51a73edefdfb94c5de4dd9b157aa22447cbea007
jGohan-cpu <jjoseriveram01@gmail.com>
2023-09-13 13:31:47 -0400

    current file changed

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
-""" 8-main """
-from pymongo import MongoClient
-list_all = __import__('8-all').list_all
+"""Script that change all topics of a school
+document based on the name"""
+import pymongo
 
-if __name__ == "__main__":
-    client = MongoClient('mongodb://127.0.0.1:27017')
-    school_collection = client.my_db.school
-    schools = list_all(school_collection)
-    for school in schools:
-        print("[{}] {}".format(school.get('_id'), school.get('name')))
+
+def update_topics(mongo_collection, name, topics):
+    """Changes all topics"""
+
+    query: dict = {"name": name}
+
+    mongo_collection.update_many(query, {"$set": {"topics": topics}})
--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
-
-""" provides some stats about Nginx logs stored in MongoDB """
+"""Script that provides some stats about Ngix
+logs stored in MongoDB"""
 import pymongo
 
 
