a25ed613fb9e04af405b3e82b707408044117be6
jGohan-cpu <jjoseriveram01@gmail.com>
2023-09-13 12:07:42 -0400

    8 fixed

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """ list all documents in a collection """
 
+
 def list_all(mongo_collection):
     """ lists all documents in a collection """
     return mongo_collection.find()
