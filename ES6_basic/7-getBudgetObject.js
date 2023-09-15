14275e3f67121df1c5e24a2e918d6548e5776f4e
jGohan-cpu <jjoseriveram01@gmail.com>
2023-09-15 17:32:30 -0400

    shorthand syntax 7

--- ES6_basic/7-getBudgetObject.js
+++ ES6_basic/7-getBudgetObject.js
@@ -1,10 +1,3 @@
-/* eslint-disable */
 export default function getBudgetObject(income, gdp, capita) {
-    const budget = {
-        income,
-        gdp,
-        capita,
-    };
-
-    return budget;
+    return { income, gdp, capita };
 }
