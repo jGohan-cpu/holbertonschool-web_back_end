f832ea97af18eda03c74bad789bc5f18fc17a065
jGohan-cpu <jjoseriveram01@gmail.com>
2023-10-09 16:13:43 -0400

    2th exercise

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,7 +1,10 @@
-/* eslint-disable */
+/* eslin-disabled */
 export default function handleResponseFromAPI(promise) {
     return promise
-        .then(() => ({ status: 200, body: 'success' }))
+        .then(() => ({
+            status: 200,
+            body: 'success',
+        }))
         .catch(() => Error())
-        .finally(() => console.log('Got a response from the API'));
+        .finally(() => console.warn('Got a response from the API'));
 }
