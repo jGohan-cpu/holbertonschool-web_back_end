dc606189849dcc850a8da9cece63212dcdfc36e8
jGohan-cpu <jjoseriveram01@gmail.com>
2023-10-11 16:49:30 -0400

    2 and 3 corrected

--- ES6_data_manipulation/2-get_students_by_loc.js
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,9 +1,9 @@
 /* enlist-disable */
 export default function getStudentsByLocation(students, city) {
-    if (!Array.isArray(students)) {
-        return [];
-    }
+  if (!Array.isArray(students)) {
+    return [];
+  }
 
-    const filter = students.filter((filteredObj) => filteredObj.location === city);
-    return filter;
+  const filter = students.filter((filteredObj) => filteredObj.location === city);
+  return filter;
 }
