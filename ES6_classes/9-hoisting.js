467ea9c6efc8e875e223f2a6073ade43c1267918
jGohan-cpu <jjoseriveram01@gmail.com>
2023-10-11 17:54:31 -0400

    spaces corrected

--- ES6_classes/9-hoisting.js
+++ ES6_classes/9-hoisting.js
@@ -1,36 +1,36 @@
 /* eslint-disable */
 export class HolbertonClass {
-    constructor(year, location) {
-        this._year = year;
-        this._location = location;
-    }
+  constructor(year, location) {
+    this._year = year;
+    this._location = location;
+  }
 
-    get year() {
-        return this._year;
-    }
+  get year() {
+    return this._year;
+  }
 
-    get location() {
-        return this._location;
-    }
+  get location() {
+    return this._location;
+  }
 }
 
 const class2019 = new HolbertonClass(2019, 'San Francisco');
 const class2020 = new HolbertonClass(2020, 'San Francisco');
 
 export class StudentHolberton {
-    constructor(firstName, lastName, holbertonClass) {
-        this._firstName = firstName;
-        this._lastName = lastName;
-        this._holbertonClass = holbertonClass;
-    }
+  constructor(firstName, lastName, holbertonClass) {
+    this._firstName = firstName;
+    this._lastName = lastName;
+    this._holbertonClass = holbertonClass;
+  }
 
-    get fullName() {
-        return `${this._firstName} ${this._lastName}`;
-    }
+  get fullName() {
+    return `${this._firstName} ${this._lastName}`;
+  }
 
-    get holbertonClass() {
-        return this._holbertonClass;
-    }
+  get holbertonClass() {
+    return this._holbertonClass;
+  }
 }
 
 const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
